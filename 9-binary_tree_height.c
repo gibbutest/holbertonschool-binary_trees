541b64cb69597e1ba98e3a53f5bef53af0055069
Ian <Gibbu_@hotmail.com>
2025-04-08 13:20:46 +1000

    stuff

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -16,6 +16,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	left_height = tree->left ? binary_tree_height(tree->left) + 1 : 0;
 	right_height = tree->right ? binary_tree_height(tree->right) + 1 : 0;
-	
+
 	return (left_height > right_height ? left_height : right_height);
 }

08f7d4fcfaa7c08e4dd1d641d84d814a81a2a146
Ian <Gibbu_@hotmail.com>
2025-04-08 13:19:36 +1000

    stuff

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Gets the height of the tree.
+ * @tree: The tree to search.
+ *
+ * Return: The height of the tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int left_height = 0;
+	int right_height = 0;
+
+	if (!tree)
+		return (0);
+
+	left_height = tree->left ? binary_tree_height(tree->left) + 1 : 0;
+	right_height = tree->right ? binary_tree_height(tree->right) + 1 : 0;
+	
+	return (left_height > right_height ? left_height : right_height);
+}

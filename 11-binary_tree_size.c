37f60e43d86565ba184b80aa481ea8b3da84181e
Ian <Gibbu_@hotmail.com>
2025-04-08 13:30:08 +1000

    stuff

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - Gets the size of each node and its children
+ * @tree: The tree/node to get the size of
+ *
+ * Return: The total size of the node
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
+}
